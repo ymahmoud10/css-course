a6ce92b1ed3677334d58dc454f61dd9460dd1f4c
ymahmoud10 <y.mahmoudm10@gmail.com>
2019-11-09 17:23:04 +0200

    responsive nav + favicon

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -11,15 +11,6 @@ $(document).ready(function() {
         offset: '60px'
     });
 
-    // /* Scroll on buttons */
-    // $('.js--scroll-to-plans').click(function() {
-    //     $('html, body').animate({scrollTop: $('.js--section-plans').offset().top - 60}, 1000)
-    // })
-
-    // $('.js--scroll-to-start').click(function() {
-    //     $('html, body').animate({scrollTop: $('.js--section-features').offset().top - 60}, 500)
-    // })
-
     /* Smooth scroll */
     $(function() {
         $('a[href*="#"]:not([href="#"])').click(function() {
@@ -59,6 +50,22 @@ $(document).ready(function() {
         $('.js--wp-4').addClass('animated pulse');
     }, {
         offset: '70%'
+    });
+
+
+    /* Mobile navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav')
+        var icon = $('.js--nav-icon i')
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round')
+            icon.removeClass('ion-navicon-round')
+        } else {
+            icon.addClass('ion-navicon-round')
+            icon.removeClass('ion-close-round')
+        }
     });
 
 });
